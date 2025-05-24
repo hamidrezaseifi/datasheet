b1a123e9206e369277a3e2b573bdedba39d075dd
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2025-05-24 15:19:21 +0200

    Create nav and ModelNavigationProvider Use menu_map for navigations

--- shared_fields/views.py
+++ shared_fields/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render, redirect
 from shared_fields.data_provider import DataProviderBase
 
 
-def generic_crud_view(request, data_provider: DataProviderBase, form_class, base_url, menu_map, primary_key_base64=None):
+def generic_crud_view(request, data_provider: DataProviderBase, form_class, base_url, menu_map,
+                      primary_key_base64=None):
+    selected_parent = get_selected_parent(menu_map, request)
 
     instance = None
     django_instance = None
@@ -25,19 +27,21 @@ def generic_crud_view(request, data_provider: DataProviderBase, form_class, base
     if request.method == 'POST' and 'delete' not in request.POST:
         print(f"POST data in generic_crud_view: {request.POST}")
         form = form_class(request.POST)
-        #form.is_valid()
+        # form.is_valid()
 
         if instance:
             data_provider.update(primary_key, request.POST)
             print(f"updated sqlalchemy: {instance}")
         else:
-            #instance = model_class(**form.cleaned_data)
-            #print(f"inserting sqlalchemy: {instance}")
+            # instance = model_class(**form.cleaned_data)
+            # print(f"inserting sqlalchemy: {instance}")
             data_provider.add(request.POST)
 
         print("redirecting to success")
-        #return redirect(f'/{base_url}/success', model_name=data_provider.get_model_title())
-        return redirect(f'/{base_url}/success', model_name=data_provider.get_nav_provider().get_model_title())
+        # return redirect(f'/{base_url}/success', model_name=data_provider.get_model_title())
+        return redirect(f'/{base_url}/success', {
+            "model_name": data_provider.get_nav_provider().get_model_title(),
+            "selected_parent": selected_parent})
 
     else:
         form = form_class(instance=django_instance)
@@ -48,15 +52,13 @@ def generic_crud_view(request, data_provider: DataProviderBase, form_class, base
                       'base_url': base_url,
                       'form': form,
                       'instance': instance,
-                      'model_name': data_provider.get_nav_provider().get_model_title()
+                      'model_name': data_provider.get_nav_provider().get_model_title(),
+                      "selected_parent": selected_parent
                   }
                   )
 
 
 def generic_delete_view(request, data_provider: DataProviderBase):
-
-    model_class = data_provider.get_data_model()
-
     if 'key' not in request.POST:
         return HttpResponseBadRequest("Invalid request. key to delete not found!", content_type="text/plain")
 
@@ -73,7 +75,6 @@ def generic_delete_view(request, data_provider: DataProviderBase):
 
 
 def generic_list_view(request, data_provider: DataProviderBase, base_url, menu_map):
-
     max_page_index = 6
 
     page_index = 1
@@ -92,15 +93,7 @@ def generic_list_view(request, data_provider: DataProviderBase, base_url, menu_m
 
     print(f"menu_map : {menu_map}")
 
-    selected_parent = None
-    for par in menu_map:
-        for proj in menu_map[par]:
-            url = request.path
-            if url.endswith("/"):
-                url = url[:len(url) - 1]
-            if url in menu_map[par][proj]:
-                selected_parent = par
-                break
+    selected_parent = get_selected_parent(menu_map, request)
 
     return render(request, 'global/list_view.html',
                   {'menu_map': menu_map,
@@ -119,6 +112,36 @@ def generic_list_view(request, data_provider: DataProviderBase, base_url, menu_m
                   )
 
 
+def generic_static_view(request, static_html, menu_map, arguments):
+    selected_parent = get_selected_parent(menu_map, request)
+
+    return render(request, static_html, {
+        'menu_map': menu_map,
+        'data': arguments,
+        "selected_parent": selected_parent})
+
+
+def success_view(request, model_name, base_url, menu_map):
+    selected_parent = get_selected_parent(menu_map, request)
+
+    return render(request, 'global/success.html',
+                  {'menu_map': menu_map,
+                   'base_url': base_url,
+                   'model_name': model_name,
+                   "selected_parent": selected_parent})
+
+
+def get_selected_parent(menu_map, request):
+    selected_parent = None
+    for par in menu_map:
+        for proj in menu_map[par]:
+            print(f"Search {menu_map[par][proj]} in {request.path}")
+            if menu_map[par][proj] in request.path:
+                selected_parent = par
+                break
+    return selected_parent
+
+
 def _generate_page_index_list(max_page_index, page_count, page_index):
     start = page_index - int(max_page_index / 2)
     end = page_index + int(max_page_index / 2)
@@ -129,12 +152,3 @@ def _generate_page_index_list(max_page_index, page_count, page_index):
         end = page_count + 1
     page_index_list = range(start, end)
     return page_index_list
-
-
-def generic_static_view(request, static_html, menu_map, arguments):
-    print("start generic_static_view for ", static_html)
-    return render(request, static_html, {'menu_map': menu_map, 'data': arguments})
-
-
-def success_view(request, model_name, base_url, menu_map):
-    return render(request, 'global/success.html', {'menu_map': menu_map, 'base_url': base_url, 'model_name': model_name})
