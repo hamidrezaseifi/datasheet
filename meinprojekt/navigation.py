f9641f2b305a8df56519f30a6ae22f8a0460b843
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2025-06-15 18:03:43 +0200

    Hamid changes

--- meinprojekt/navigation.py
+++ meinprojekt/navigation.py
@@ -1,19 +1,17 @@
-
-
 NAVIGATION_DATA = [
-    {"name": "Start-Seite", "url": "/", "children": []},
-    {"name": "Meine Test", "url": "#", "children": [
-        {"name": "Eingabe", "url": "/eingabe", "children": []},
-        {"name": "Planung", "url": "/planung", "children": []},
+    {"id": 1, "parent": 0, "name": "Start-Seite", "url": "/", "children": []},
+    {"id": 2, "parent": 0, "name": "Meine Test", "url": "#", "children": [
+        {"id": 3, "parent": 2, "name": "Eingabe", "url": "/eingabe", "children": []},
+        {"id": 4, "parent": 2, "name": "Planung", "url": "/planung", "children": []},
     ]},
-    {"name": "Sales", "url": "#", "children": [
-        {"name": "Formular", "url": "#", "children": [
-            {"name": "Objekte", "url": "/sales/objekte", "children": []},
-            {"name": "Prognose", "url": "/sales/prognose", "children": []},
+    {"id": 5, "parent": 0, "name": "Sales", "url": "#", "children": [
+        {"id": 6, "parent": 5, "name": "Formular", "url": "#", "children": [
+            {"id": 7, "parent": 6, "name": "Objekte", "url": "/sales/objekte", "children": []},
+            {"id": 8, "parent": 6, "name": "Prognose", "url": "/sales/prognose", "children": []},
         ]},
-        {"name": "Report", "url": "#", "children": [
-            {"name": "Report 1", "url": "#", "children": []},
-            {"name": "Report 2", "url": "#", "children": []},
+        {"id": 9, "parent": 5, "name": "Report", "url": "#", "children": [
+            {"id": 10, "parent": 9, "name": "Report 1", "url": "#", "children": []},
+            {"id": 11, "parent": 9, "name": "Report 2", "url": "#", "children": []},
         ]},
 
     ]},
