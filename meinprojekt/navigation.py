b6eba1b1e750f980c333b74d7889d3326cd4a293
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2025-06-16 12:13:00 +0200

    Hamid changes

--- meinprojekt/navigation.py
+++ meinprojekt/navigation.py
@@ -7,8 +7,14 @@ root_items = [i for i in all_nv_items if i["parent"] == 0]
 
 def process_nav(item, all_items):
     children = [process_nav(i, all_items) for i in all_items if i["parent"] == item["id"]]
-    return {"id": item["id"], "parent": item["parent"], "name": item["nav_name"], "url": item["url"],
-            "children": children}
+    nav_item = {"id": item["id"], "parent": item["parent"], "name": item["nav_name"],
+                "start_page": item["start_page"] == 1, "last_parent": False,
+                "url": item["url"], "children": children}
+
+    if len(nav_item["children"]) > 0:
+        lst = [i for i in nav_item["children"] if len(i["children"]) > 0]
+        nav_item["last_parent"] = len(lst) > 0
+    return nav_item
 
 
 NAVIGATION_DATA = []
--- meinprojekt/navigation.sql
+++ meinprojekt/navigation.sql
@@ -4,19 +4,20 @@ CREATE TABLE djange.navigation (
 	parent int4 NOT NULL,
 	nav_name varchar(255) NOT NULL,
 	url varchar(255) DEFAULT '#'::character varying NOT NULL,
+	start_page int2 DEFAULT 1 NOT NULL,
 	CONSTRAINT navigation_pk PRIMARY KEY (id)
 );
 
 INSERT INTO djange.navigation (id,parent,nav_name,url) VALUES
-	 (1,0,'Start-Seite','/'),
-	 (2,0,'Meine Test','#'),
-	 (3,2,'Eingabe','/eingabe'),
-	 (4,2,'Planung','/planung'),
-	 (5,0,'Sales','#'),
-	 (6,5,'Formular','#'),
-	 (7,6,'Objekte','/sales/objekte'),
-	 (8,6,'Prognose','/sales/prognose'),
-	 (9,5,'Report','#'),
-	 (10,9,'Report 1','#');
+	 (1,0,'Start-Seite','/',0),
+	 (2,0,'Meine Test','#',1),
+	 (3,2,'Eingabe','/eingabe',1),
+	 (4,2,'Planung','/planung',1),
+	 (5,0,'Sales','#',1),
+	 (6,5,'Formular','#',1),
+	 (7,6,'Objekte','/sales/objekte',1),
+	 (8,6,'Prognose','/sales/prognose',1),
+	 (9,5,'Report','#',1),
+	 (10,9,'Report 1','#',1);
 INSERT INTO djange.navigation (id,parent,nav_name,url) VALUES
-	 (11,9,'Report 2','#');
+	 (11,9,'Report 2','#',1);
